5fe94c5e62b4b80b398a362f7add0c9daa83e2a2
matinanadali <matinad@gmail.com>
2024-12-30 23:41:56 +0200

    fix close door logic

--- src/State/Door.cpp
+++ src/State/Door.cpp
@@ -4,6 +4,7 @@
 #include "State/Button.h"
 #include "General.h"
 #include "Math/Vector2.h"
+#include "State/Player.h"
 
 Door::Door(const std::string& doorName, Vector2 position, Vector2 startingSize, 
 const std::string& newSrcName, Vector2 newSourceRectanglePosition, Vector2 newSourceRectangleSize,
@@ -59,6 +60,16 @@ void Door::Open()
         bCollides = false;
     }
 }
+
+bool isPositionValid(Object* player) {
+    for (const auto &[name, object] : State::GetAllObjects()) {
+        if (object != player && object->bCollides && State::AreObjectsOverlapping(object, player)) {
+            return false;
+        }
+    }
+    return true;
+}
+
 void Door::Close()
 {
     if(bIsOpen)
@@ -67,4 +78,41 @@ void Door::Close()
         bIsOpen = false;
         bCollides = true;
     }
+
+    Object* player1 = State::GetAllObjects()["Santa Male"];
+    Object* player2 = State::GetAllObjects()["Santa Female"];
+    Object* player = NULL;
+    
+    if (State::AreObjectsOverlapping(this, player1)) {
+        player = player1;
+    } else if (State::AreObjectsClose(this, player2)) {
+        player = player2;
+    }
+
+    if (player == NULL) return;
+    
+    float oldY = player->position.y;
+
+    player->position.y = position.y - player->size.y;
+    if (isPositionValid(player)) {
+        return;
+    }
+
+    player->position.y = position.y + size.y;
+    if (isPositionValid(player)) {
+        return;
+    }
+
+    player->position.y = oldY;
+    player->position.x = position.x - player->size.x;
+    if (isPositionValid(player)) {
+        return;
+    }
+
+    player->position.x = position.x + size.x;
+    if (isPositionValid(player)) {
+        return;
+    }
+
+    
 }
